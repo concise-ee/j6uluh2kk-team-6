8c565edeb7f2df29df62d293f77834d741ad88ed
Marco Lackovic <marco.lackovic@gmail.com>
2021-12-07 14:23:28 +0200

    Change header text

--- my-app/components/Header.js
+++ my-app/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const Header = () => {
     return(
         <div className="header">
-            <h1>Concise j6uluh2kk</h1>
+            <h1>Häid jõule 6. tiimist</h1>
         </div>
     )
 }
