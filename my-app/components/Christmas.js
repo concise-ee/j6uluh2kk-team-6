9b722046ade99474ad6a8fb92b8fd3fc4c355b1c
Marco Lackovic <marco.lackovic@gmail.com>
2021-12-07 16:24:05 +0200

    Play Last Christmas song on button press

--- my-app/components/Christmas.js
+++ my-app/components/Christmas.js
@@ -1,13 +1,20 @@
 import React, { useState } from 'react';
 import YoutubeEmbed from './YoutubeEmbed';
+import Button from '@mui/material/Button';
 
-export const Christmas = ({christmas}) => {
-    return(
-        <div className="container">
-            <img src="baby-yoda.jpg" width="100%"/>
-            
+export const Christmas = () => {
+    const [pressed, setPressed] = useState(false);
+    return (
+        <div className="container" align="center">
+            {pressed ? <>
+                <img src="merry-christmas.gif" width="100%" />
+                <Button variant="contained" color="error" onClick={() => setPressed(false)}>Stop</Button>
+            </> : <>
+                <img src="baby-yoda.jpg" width="100%" />
+                <Button variant="contained" color="error" onClick={() => setPressed(true)}>Claim your present here</Button>
+            </>}
             <div className={'row'} width={'100%'} hidden={true}>
-                <YoutubeEmbed embedId={'E8gmARGvPlI'}/>
+                <YoutubeEmbed embedId={'E8gmARGvPlI'} autoplay={pressed ? 1 : 0} />
             </div>
         </div>
     )
--- my-app/components/YoutubeEmbed.js
+++ my-app/components/YoutubeEmbed.js
@@ -1,12 +1,12 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const YoutubeEmbed = ({ embedId }) => (
+const YoutubeEmbed = ({ embedId, autoplay }) => (
     <div className="video-responsive">
         <iframe
             width="853"
             height="480"
-            src={`https://www.youtube.com/embed/${embedId}?autoplay=1`}
+            src={`https://www.youtube.com/embed/${embedId}?autoplay=${autoplay}&loop=1`}
             frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen
