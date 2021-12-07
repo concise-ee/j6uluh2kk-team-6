3871c5ce34b789b377616870c4bbd2565de9079b
Andro Põlluste <andro.polluste@easypark.net>
2021-12-07 16:01:28 +0200

    song

--- my-app/components/Christmas.js
+++ my-app/components/Christmas.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react';
+import YoutubeEmbed from './YoutubeEmbed';
 
 export const Christmas = ({christmas}) => {
     return(
         <div className="container">
             <img src="baby-yoda.jpg" width="100%"/>
+            
+            <div className={'row'} width={'100%'} hidden={true}>
+                <YoutubeEmbed embedId={'E8gmARGvPlI'}/>
+            </div>
         </div>
     )
 }
